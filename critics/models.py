f31ed39ab5cc0ec99b5d32794dc9d7ae0da71f9b
hosseinmohamadii <156761865+hosseinmohamadii@users.noreply.github.com>
2024-03-15 11:34:15 +0330

    ..

--- critics/models.py
+++ critics/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 class Critic(models.Model):
-    title = models.CharField(max_length=200)
-    movie_name = models.CharField(max_lenght=150)
+    title = models.CharField(max_lenght=255)
+    movie_name = models.CharField(max_lenght=255)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
